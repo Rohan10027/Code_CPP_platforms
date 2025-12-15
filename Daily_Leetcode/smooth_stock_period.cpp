c1ff8ae1a9c4bf83c5da30c0dfc5dabd069f09d2
Rohan Baranwal <90006085+Rohan10027@users.noreply.github.com>
2025-12-15 22:17:46 +0530

    leet_dec15
    ok

---
+++ Daily_Leetcode/smooth_stock_period.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long getDescentPeriods(vector<int>& prices) {
+        int *dp = new int[prices.size()];
+        dp[0] = 1;
+        long long ans = 1;
+        for(int i = 1; i < prices.size(); i ++) {
+            if(prices[i] == prices[i - 1] - 1) {
+                dp[i] = dp[i - 1] + 1;
+            } else {
+                dp[i] = 1;
+            }
+            ans += dp[i];
+        }
+        return ans;
+    }
+};
