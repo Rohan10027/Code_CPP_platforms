dc2e793aa8c6bfb85c9fe841a335f576070d5186
Rohan Baranwal <90006085+Rohan10027@users.noreply.github.com>
2026-01-20 09:39:45 +0530

    leet_jan20
    ok

---
+++ Daily_Leetcode/leet_jan20.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+        vector<int> minBitwiseArray(vector<int>& A) {
+        vector<int> res;
+        for (int a : A) {
+            if (a % 2 == 0) {
+                res.push_back(-1);
+            } else {
+                res.push_back(a - ((a + 1) & (-a - 1)) / 2);
+            }
+        }
+        return res;
+    }
+};
