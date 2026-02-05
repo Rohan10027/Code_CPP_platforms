5f1bc602e837d333c66389511a2e75636983073f
Rohan Baranwal <90006085+Rohan10027@users.noreply.github.com>
2026-02-05 09:16:31 +0530

    gfg_feb05 (#258)
    ok

---
+++ Daily_GFG/max_ones.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    virtual int maxOnes(vector<int>& arr, int k) {
+      int maxi=0, c0=0, i=0, j=0;
+      
+      int n=arr.size();
+      
+      for(j=0; j<n; j++){
+        if(arr[j]==0) c0++;
+        while(c0>k){
+          if(arr[i]==0) c0--;
+          i++;
+        }
+        
+        maxi = max(maxi, j-i+1);
+      }
+      
+      return maxi;
+    }
+};
