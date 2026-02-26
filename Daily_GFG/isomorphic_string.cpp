3f602b6358934dd66ba73733761787ee84668bc4
Rohan Baranwal <90006085+Rohan10027@users.noreply.github.com>
2026-02-26 08:46:51 +0530

    gfg_feb26
    ok

---
+++ Daily_GFG/isomorphic_string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    bool areIsomorphic(string &s1, string &s2) {
+        // code here
+        int n=s1.size();
+        map<char,char>mp1,mp2;
+        for(int i=0;i<n;i++){
+            if(mp1.find(s1[i])!=mp1.end() && mp1[s1[i]]!=s2[i]) return false;
+            if(mp2.find(s2[i])!=mp2.end() && mp2[s2[i]]!=s1[i]) return false;
+            mp1[s1[i]]=s2[i];
+            mp2[s2[i]]=s1[i];
+        }
+        return true;
+    }
+};
