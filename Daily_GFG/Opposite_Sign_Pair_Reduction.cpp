25ca65c090021fc263c0ebe2ab41390b3b22c632
Rohan Baranwal <90006085+Rohan10027@users.noreply.github.com>
2026-04-25 07:53:31 +0530

    gfg_apr25 (#417)
    Implement a solution to reduce pairs of opposite signs in an array.

---
+++ Daily_GFG/Opposite_Sign_Pair_Reduction.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    vector<int> reducePairs(vector<int>& arr) {
+        vector<int> st;
+        
+        for (int x : arr) {
+            bool alive = true;
+            
+            while (!st.empty() && alive && (st.back() * x < 0)) {
+                int top = st.back();
+                
+                if (abs(top) == abs(x)) {
+                    st.pop_back();
+                    alive = false;
+                } else if (abs(top) > abs(x)) {
+                    alive = false;
+                } else {
+                    st.pop_back();
+                }
+            }
+            
+            if (alive) st.push_back(x);
+        }
+        
+        return st;
+    }
+};
